d7569933620eb580e8f5c46c6d51afc0ea1b1e03
Erdem <zahrakon06@gmail.com>
2023-11-06 21:42:50 +0300

    .

--- src/lib/ExpressHandler.ts
+++ src/lib/ExpressHandler.ts
@@ -1,9 +1,13 @@
 import { App } from "./App";
 import { Express } from "express";
+import * as fs from "fs/promises";
 
 export class ExpressHandler {
   public router: Express;
-  constructor(private app: App) {}
+  constructor(private app: App) {
+    // ignore this, its just to make tsc aware of to import tslib, otherwise it doesnt and code doesnt run
+    fs;
+  }
 
   async init() {
     const express = await import("express").then(m => m?.default);
