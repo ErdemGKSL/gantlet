2d77907c24ae5e8f9345ade725039f90b8f35f2c
Erdem <zahrakon06@gmail.com>
2023-11-08 18:29:40 +0300

    .

--- src/lib/Middleware.ts
+++ src/lib/Middleware.ts
@@ -2,8 +2,15 @@ import type { App } from "./App";
 import type { Handler } from "./HttpServer";
 
 export class Middleware {
-  
-
   constructor(private app: App, public path: string[], public handler: Handler) {}
 
+  toJSON() {
+    return {
+      method: "MIDDLEWARE",
+      path: this.path
+        .filter(p => !(p.startsWith("(") && p.endsWith(")")))
+        .map((p) => (p.startsWith("[") && p.endsWith("]")) ? `:${p.slice(1, -1)}` : p)
+        .join("/")
+    }
+  }
 }
--- src/lib/Route.ts
+++ src/lib/Route.ts
@@ -2,10 +2,17 @@ import type { App } from "./App";
 import type { Handler } from "./HttpServer";
 
 export class Route {
-  
-
   constructor(private app: App, public path: string[], public method: RouteMethods, public handler: Handler) {}
 
+  toJSON() {
+    return {
+      method: this.method,
+      path: this.path
+        .filter(p => !(p.startsWith("(") && p.endsWith(")")))
+        .map((p) => (p.startsWith("[") && p.endsWith("]")) ? `:${p.slice(1, -1)}` : p)
+        .join("/")
+    }
+  }
 }
 
 export type RouteMethods = "GET" | "POST" | "PUT" | "DELETE" | "PATCH" | "OPTIONS" | "HEAD" | "CONNECT" | "TRACE" | "ALL";
